944160888255b44195a2381ca62e456597b72455
plotia <plotia@andrew.cmu.edu>
2024-10-13 17:52:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function toggleSubNav(subNavId) {
+    var subNav = document.getElementById(subNavId);
+    var parentLi = subNav.parentElement;
+
+    // Toggle the open class to show or hide sub-navigation
+    parentLi.classList.toggle("open");
+
+    // Hide other open sub-navs (if any)
+    document.querySelectorAll("nav ul > li.open").forEach(openLi => {
+        if (openLi !== parentLi) {
+            openLi.classList.remove("open");
+        }
+    });
+}
+
+// Function to change content based on the selected page
+function changeContent(page) {
+    var contentDiv = document.getElementById('content');
+
+    switch (page) {
+        case 'home':
+            window.location.href = "homepage.html"
+            break;
+        case 'Research':
+            window.location.href = "research.html";
+            break;
+        case 'Experience':
+            window.location.href = "projects.html"
+        case 'Projects':
+            window.location.href = "projects.html"
+            break;
+        default:
+            contentDiv.innerHTML = '<h2>Page not found!</h2>';
+    }
+}
